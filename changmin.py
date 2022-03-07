1958907c0e063f5333b6160aee73b135630e77eb
gleaten <46469581+gleaten@users.noreply.github.com>
2022-03-07 20:05:30 +0900

    재훈이코드 수정+내파일 삭제

--- changmin.py
+++
@@ -1,9 +0,0 @@
-class changmin:
-    def __init__(self) -> None:
-        pass
-    def introduce(self):
-        print("changmin")
-
-if __name__ == '__main__':
-    person = changmin()
-    person.introduce()
