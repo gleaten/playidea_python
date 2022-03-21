ec6a470e7286794c8f9004bc654c44528fc97526
gleaten <46469581+gleaten@users.noreply.github.com>
2022-03-21 14:49:33 +0900

    iot 3주차

---
+++ lectures/22_1_IOT/practice3.py
@@ -0,0 +1,13 @@
+
+# print("hello world")
+
+# Rx_123123 변수 이름 
+
+x = 1.1 # 1, 1.1, None, True, False
+x = "stratwetwtwet"
+x = (1,2,3,1,)
+x = [1,2,3,1,]
+x = {112,31,1}
+x = {"cm":35,"hj":32}
+
+x[]
