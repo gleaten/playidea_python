11f7b3bc2fbcc571a02ea0b76a0200a918831a8d
gleaten <46469581+gleaten@users.noreply.github.com>
2022-04-13 16:25:27 +0900

    이것저것 업데이트- 개발환경 꼬여서 다시 함

--- lectures/22_1_SW/lottery_pr.py
+++ lectures/22_1_SW/lottery_pr.py
@@ -1,10 +1,25 @@
-import random
-import pandas as pd
-df_sheet_index = pd.read_excel('./출석부.xls')
-print(df_sheet_index)
 
-# random.seed(4478)
+if __name__=='__main__':
+    import random
+    import pandas as pd
+    
+    df_sheet_index = pd.read_excel('./lectures/22_1_SW/출석부.xls')
+    gwa = df_sheet_index["학과"].to_list()
+    name = df_sheet_index["성명"].to_list()
+    N = len(gwa)
 
+    seed_ = 4478
+    random.seed(seed_)
+    teams = list(range(1,N+1))  
+
+    random.shuffle(teams)
+
+    for ind, (g,n) in enumerate(zip(gwa,name)):
+        if ind < N/2:
+            print(f"오늘 발표 {ind+1} 번째 = {g} {n[:-1]}* ")
+        else: 
+            print(f"다음 발표 {ind-(N//2)} 번째 ={g} {n[:-1]}*")
+    
 # teams = list(range(1,40))
 
 # random.shuffle(teams)
